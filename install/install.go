ee21aa10423e5a69bfbcc18f803015d028a50e64
Eswar Rajan Subramanian <eswar@accuknox.com>
2022-06-09 15:58:07 +0530

    Correcting and adding proper image name
    Signed-off-by: Eswar Rajan Subramanian <eswar@accuknox.com>

--- install/install.go
+++ install/install.go
@@ -105,7 +105,7 @@ func GetDeployment(namespace string) *appsv1.Deployment {
 					Containers: []corev1.Container{
 						{
 							Name:            "knoxautopolicy",
-							Image:           "seswarrajan/knoxautopolicy:dev",
+							Image:           "accuknox/knoxautopolicy:dev",
 							ImagePullPolicy: "Always",
 							Ports: []corev1.ContainerPort{
 								{
